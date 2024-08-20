2b08bf1628abab4706e7d29c4067e7b5bc272e42
lvlcnapps <57109390+lvlcnapps@users.noreply.github.com>
2024-08-20 18:52:16 +0300

    fix pr 1

--- moad_bet/problem_1.py
+++ moad_bet/problem_1.py
@@ -2,8 +2,12 @@ def find_max(lst) -> int:
     if len(lst) == 0:
         return -1000000
     if type(lst[0]) == int:
+        if len(lst) == 1:
+            return lst[0]
         return max(lst[0], find_max(lst[1:]))
     else:
+        if len(lst) == 1:
+            return find_max(lst[0])
         return max(find_max(lst[0]), find_max(lst[1:]))
 
 print(find_max([1, 3, [4, 5, [10, 50]], 2]))
